2f310ff012846883bb48ec95b7897bbdb145d591
KacperG98 <kandmgamestudio@gmail.com>
2023-04-15 11:08:06 +0200

    zajęcia 1

---
+++ Laboratory-1/Program.cs
@@ -0,0 +1,23 @@
+﻿int BinarySearch(int target, List<int> collection)
+{
+    
+    int halfOfList = collection.Count / 2;
+
+    if (collection[halfOfList] < target)
+    {
+        return BinarySearch(target, collection.Skip(halfOfList).ToList());
+    }
+    else if (collection[halfOfList] > target)
+    {
+        return BinarySearch(target, collection.Take(halfOfList).ToList());
+    }
+    return collection[halfOfList];
+}
+
+
+var RangeList = Enumerable.Range(1, 100).ToList();
+Console.WriteLine("Podaj liczbę jaką mam znaleźć z zagresu 1-100:");
+
+int NumberToFind = Int32.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine("Znaleziona liczba to: {0}", BinarySearch(NumberToFind, RangeList));
